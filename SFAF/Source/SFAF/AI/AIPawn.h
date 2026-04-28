ef603da05d7134057af91de090fc6d6d53fb05df
Gabriel Nobile Diniz <gabrielnd@gmail.com>
2026-04-28 18:09:51 -0300

    Refactor: Name and Year

--- SFAF/Source/SFAF/AI/AIPawn.h
+++ SFAF/Source/SFAF/AI/AIPawn.h
@@ -1,4 +1,4 @@
-// © 2024 Gabriel Nóbile Diniz. All Rights Reserved.This software and its content, including but not limited to code, art, assets, and documentation, are the exclusive property of Gabriel Nóbile Diniz. Unauthorized copying, distribution, adaptation, or other use is prohibited without explicit permission.For inquiries or permission requests, please contact hearnodarkness@gmail.com.
+// ï¿½ 2026 Gabriel Nobile Diniz. All Rights Reserved.This software and its content, including but not limited to code, art, assets, and documentation, are the exclusive property of Gabriel Nï¿½bile Diniz. Unauthorized copying, distribution, adaptation, or other use is prohibited without explicit permission.For inquiries or permission requests, please contact hearnodarkness@gmail.com.
 
 #pragma once
 
